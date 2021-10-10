2994d9825e02eed60705aaf72405ea2e4a0574d4
skk <skkdetroit@gmail.com>
2021-10-09 23:16:01 -0400

    update APPmjs with provider details

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 # BookSearch
+
 AS AN avid reader
 I WANT to search for new books to read
 SO THAT I can keep a list of books to purchase
+
 ```
 
 ## Acceptance Criteria
--- client/src/App.js
+++ client/src/App.js
@@ -3,19 +3,42 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
+// Adding Apollo
+import { ApolloProvider } from '@apollo/react-hooks';
+import ApolloClient from 'apollo-boost';
+
+// Connecting to graphql
+const client = new ApolloClient({
+  // uri: 'http://localhost:3001/graphql'
+  request: operation => {
+    const token = localStorage.getItem('id_token');
+
+    operation.setContext({
+      headers: {
+        authorization: token ? `Bearer ${token}` : ''
+      }
+    })
+  },
+  uri: '/graphql'
+});
+
 
 function App() {
   return (
-    <Router>
+    <ApolloProvider client={client}>
+    {/* <Router> */}
       <>
         <Navbar />
         <Switch>
+
           <Route exact path='/' component={SearchBooks} />
           <Route exact path='/saved' component={SavedBooks} />
           <Route render={() => <h1 className='display-2'>Wrong page!</h1>} />
+
         </Switch>
       </>
-    </Router>
+    {/* </Router> */}
+      </ApolloProvider>
   );
 }
 
