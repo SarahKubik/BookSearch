ff247a7d1991f08b5531efebe5ded1583203c734
skk <skkdetroit@gmail.com>
2021-10-09 22:55:41 -0400

    schemas

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -8,6 +8,7 @@ module.exports = {
   // function for our authenticated routes
   authMiddleware: function (req, res, next) {
     // allows token to be sent via  req.query or headers
+
     let token = req.query.token || req.headers.authorization;
 
     // ["Bearer", "<tokenvalue>"]
@@ -35,5 +36,7 @@ module.exports = {
     const payload = { username, email, _id };
 
     return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
+
+   
   },
 };
