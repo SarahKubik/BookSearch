9a4606c3f0eb5e633c98a05d5c628cf7684404d0
skk <skkdetroit@gmail.com>
2021-10-11 14:39:31 -0400

    updated pages

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -1,13 +1,10 @@
 import React, { useState, useEffect } from 'react';
+import { useMutation } from '@apollo/react-hooks';
 import { Jumbotron, Container, Col, Form, Button, Card, CardColumns } from 'react-bootstrap';
-
 import Auth from '../utils/auth';
 import { searchGoogleBooks } from '../utils/API';
 import { saveBookIds, getSavedBookIds } from '../utils/localStorage';
-
-// Add Apollo dependencies
 import { SAVE_BOOK } from '../utils/mutations';
-import {useMutation} from '@apollo/react-hooks';
 
 const SearchBooks = () => {
   // create state for holding returned google api data
@@ -18,13 +15,8 @@ const SearchBooks = () => {
   // create state to hold saved bookId values
   const [savedBookIds, setSavedBookIds] = useState(getSavedBookIds());
 
-  const [saveBook, {error}] = useMutation(SAVE_BOOK );
-
   // set up useEffect hook to save `savedBookIds` list to localStorage on component unmount
   // learn more here: https://reactjs.org/docs/hooks-effect.html#effects-with-cleanup
-
-
-
   useEffect(() => {
     return () => saveBookIds(savedBookIds);
   });
@@ -61,8 +53,10 @@ const SearchBooks = () => {
     }
   };
 
-  // create function to handle saving a book to our database
+  const [saveBook] = useMutation(SAVE_BOOK);
+
   const handleSaveBook = async (bookId) => {
+
     // find the book in `searchedBooks` state by the matching id
     const bookToSave = searchedBooks.find((book) => book.bookId === bookId);
 
@@ -74,19 +68,12 @@ const SearchBooks = () => {
     }
 
     try {
-      // const response = await saveBook(bookToSave, token);
+      console.log('bookToSave:', bookToSave);
 
-      // if (!response.ok) {
-      //   throw new Error('something went wrong!');
-      // }
-
-      const {data} = await saveBook({
-        variables: { input: bookToSave }
+      const { data } = await saveBook({
+        variables: { bookData: { ...bookToSave } }
       });
-
-      if (error) {
-        throw new Error('something went wrong!');
-      }
+      console.log('data:', data);
 
       // if book successfully saves to user's account, save book id to state
       setSavedBookIds([...savedBookIds, bookToSave.bookId]);
--- server/server.js
+++ server/server.js
@@ -1,32 +1,26 @@
-import { ApolloServer } from 'apollo-server-express';
-import { ApolloServerPluginDrainHttpServer } from 'apollo-server-core';
-import express from 'express';
-import http from 'http';
+const express = require('express');
+// import Apollo Server
+const { ApolloServer } = require('apollo-server-express');
+const path = require('path');
 
-async function startApolloServer(typeDefs, resolvers) {
-  // Required logic for integrating with Express
-  const app = express();
-  const httpServer = http.createServer(app);
+// import typedefs and resolvers
+const { typeDefs, resolvers } = require('./schema');
+const db = require('./config/connection');
+const { authMiddleware } = require('./utils/auth');
 
-  // Same ApolloServer initialization as before, plus the drain plugin.
-  const server = new ApolloServer({
-    typeDefs,
-    resolvers,
-    plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
-  });
+const app = express();
+const PORT = process.env.PORT || 3001;
 
-  // More required logic for integrating with Express
-  await server.start();
-  server.applyMiddleware({
-     app,
+// create a new Apollo and pass in the schema data
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware
+});
 
-     // By default, apollo-server hosts its GraphQL endpoint at the
-     // server root. However, *other* Apollo Server packages host it at
-     // /graphql. Optionally provide this to match apollo-server.
-     path: '/'
-  });
+server.applyMiddleware({ app });
 
-  app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 // if we're in production, serve client/build as static assets
@@ -38,67 +32,11 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
 
-  // Modified server startup
-  await new Promise(resolve => httpServer.listen({ port: 4000 }, resolve));
-  console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`);
-}
-
-
-
-// const express = require('express');
-// const path = require('path');
-// const db = require('./config/connection');
-// // const routes = require('./routes');
-
-// // Adding dependencies
-// const { ApolloServer } = require('apollo-server-express');
-// const { authMiddleware } = require('./utils/auth');
-// const { typeDefs, resolvers } = require('./schemas');
-
-
-// const app = express();
-// const PORT = process.env.PORT || 3001;
-
-// // Adding Apollo
-// const server = new ApolloServer({
-//   typeDefs,
-//   resolvers,
-//   context: authMiddleware
-// })
-
-// // Middleware
-// server.applyMiddleware({app});
-
-// app.use(express.urlencoded({ extended: true }));
-// app.use(express.json());
-
-// // if we're in production, serve client/build as static assets
-// if (process.env.NODE_ENV === 'production') {
-//   app.use(express.static(path.join(__dirname, '../client/build')));
-// }
-
-// app.get('*', (req, res) => {
-//   res.sendFile(path.join(__dirname, '../client/build/index.html'));
-// });
-
-// app.use(routes);
-
-// db.once('open', () => {
-//   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
-// });
 
 db.once('open', () => {
   app.listen(PORT, () => {
-    console.log(`API server running on port ${PORT}!`);
+    console.log(`\n ----------- 🚀 API server running on port ${PORT}! -----------`);
     // log where we can go to test our GQL API
-    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
-
+    console.log(`\n ----------- Use GraphQL at http://localhost:${PORT}${server.graphqlPath} ----------`);
   });
 });
-
-// For any uncaught or unhandled exceptions
-process.on('uncaughtException', function(err) {
-
-	// Handle the error safely
-	console.log(err)
-})
