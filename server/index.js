345e1f600451723fb57fde491c59dddbb7cd2d06
skk <skkdetroit@gmail.com>
2021-10-11 16:24:01 -0400

    update index.js

--- server/index.js
+++ server/index.js
@@ -79,7 +79,10 @@ if (process.env.NODE_ENV === 'production') {
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
-app.listen(4000, function () {
-    console.log(`server running on port 4000`);
-    console.log(`gql path is ${apolloServer.graphqlPath}`);
+db.once('open', () => {
+  app.listen(PORT, () => {
+    console.log(`\n ----------- 🚀 API server running on port ${PORT}! -----------`);
+    // log where we can go to test our GQL API
+    console.log(`\n ----------- Use GraphQL at http://localhost:${PORT}${server.graphqlPath} ----------`);
+  });
 });
