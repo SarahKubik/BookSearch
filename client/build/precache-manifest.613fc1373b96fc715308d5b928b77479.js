74ae9218fc1d90cde1425c6cc47bc71da76598ef
skk <skkdetroit@gmail.com>
2021-10-12 19:34:30 -0400

    updated react front end route

---
+++ client/build/precache-manifest.613fc1373b96fc715308d5b928b77479.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e5e5c3e4244b56ab23216d8a510249f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "99787f47e60f90477789",
+    "url": "/static/css/2.63f31f8d.chunk.css"
+  },
+  {
+    "revision": "6f8693da05d3a02b71af",
+    "url": "/static/css/main.a13d3d6a.chunk.css"
+  },
+  {
+    "revision": "99787f47e60f90477789",
+    "url": "/static/js/2.239c736e.chunk.js"
+  },
+  {
+    "revision": "4ebd2e1c8aec2794a625d08d8b03eea9",
+    "url": "/static/js/2.239c736e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f8693da05d3a02b71af",
+    "url": "/static/js/main.126e5cf8.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
